350fdf9198e467f2f2a1b6a436ba092cf9887b98
Anna <annadmitrievna1823@gmail.com>
2018-02-27 12:07:58 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    if(str.length % 2 !== 0){
+        return false;
+    } else {
+
+        for(let i = str.length - 1; i >= 0; i--){
+            for (let a = 0; a < bracketsConfig.length; a++) {
+                if (str[i] === bracketsConfig[a][0]) {
+                    str = str.replace(bracketsConfig[a].join(''), '');
+                }
+            }
+        }
+
+
+        return str === ''; // return (str === '' ) ? true : false ;
+    }
+};
+
